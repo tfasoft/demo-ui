cf7bff0787de294a188f576d12f799ba7e55d906
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-24 17:01:40 +0430

    Add panel routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createTheme, ThemeProvider, colors, CssBaseline } from "@mui/material";
 
 import Navbar from "./component/navbar";
-import Home from "./pages/home";
+import HomePage from "./pages/home";
 import AuthPage from "./pages/auth";
+import PanelPage from "./pages/panel";
 
 function App() {
     const theme = createTheme({
@@ -22,8 +23,9 @@ function App() {
             <Router>
                 <Navbar />
                 <Switch>
-                    <Route path='/' exact><Home /></Route>
+                    <Route path='/' exact><HomePage /></Route>
                     <Route path='/auth' exact><AuthPage /></Route>
+                    <Route path='/panel' exact><PanelPage /></Route>
                 </Switch>
             </Router>
         </ThemeProvider>
