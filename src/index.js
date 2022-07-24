d5921a5680fdb59938b5961be429e31a2f38f106
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-24 16:26:47 +0430

    Create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+import {loadState, saveState} from "./redux/store/localstore";
+import reducers from "./redux/reducers";
+
 import App from './App';
 import "./App.sass";
 
+const persistedState = loadState();
+
+let store = createStore(
+    reducers,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+store.subscribe(() => saveState({
+    env: store.getState().env
+}))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
