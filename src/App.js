111efb10f3c1065322a71f365d57878f898c6b10
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-24 13:09:10 +0430

    Add some stuff like router, theme and etc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createTheme, ThemeProvider, colors, CssBaseline } from "@mui/material";
+import Navbar from "./component/navbar";
+
 function App() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: colors.indigo[500],
+            },
+            white: {
+                main: "#ffffff",
+            }
+        }
+    });
+
     return (
-        <h1>Hi</h1>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact>Index</Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
     );
 }
 
