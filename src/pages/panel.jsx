731554408f172d9df327068cbb0589c7f7e82ef2
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-25 16:50:43 +0430

    Almost done in tfa adding

--- src/pages/panel.jsx
+++ src/pages/panel.jsx
@@ -20,6 +20,7 @@ import {
     Snackbar,
     Alert,
 } from "@mui/material";
+import {createUser} from "../redux/actions/user";
 
 const PanelPage = () => {
     const dispatch = useDispatch();
@@ -27,6 +28,45 @@ const PanelPage = () => {
 
     const session = useSelector(state => state.session);
     if (!session) history.push('/auth');
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        Axios.get(`http://localhost:8000/users/${uid}`)
+            .then((result) => {
+                dispatch(createUser(result.data));
+                setUser(result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []);
+
+    const uid = useSelector(state => state.uid);
+
+    const telegramAuth = user.tid;
+
+    const [telegramID, setTelegramID] = useState('');
+    const [openTelegramID, setOpenTelegramID] = useState(false);
+
+    const enableTFA = () => {
+        const newData = {
+            "username": user.username,
+            "password": user.password,
+            "tid": telegramID,
+        }
+
+        Axios.put(`http://localhost:8000/users/${uid}`, newData)
+            .then((result) => {
+                dispatch(createUser(result.data));
+                setUser(result.data);
+
+                setOpenTelegramID(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
     return (
         <Container
@@ -46,6 +86,62 @@ const PanelPage = () => {
                     borderColor: "primary.main"
                 }}
             />
+            <br />
+            <Typography
+                variant="h5"
+                color={ telegramAuth ? 'success.main' : 'error.main' }
+                gutterBottom
+            >
+                { telegramAuth ? 'Telegram authentication is activated.' : 'Telegram authentication is not activated.' }
+            </Typography>
+            {
+                !telegramAuth
+                &&
+                <Button
+                    variant="contained"
+                    color="success"
+                    onClick={() => setOpenTelegramID(true)}
+                    disableElevation
+                >
+                    Activate Telegram Authentication
+                </Button>
+            }
+
+            <Dialog
+                open={openTelegramID}
+                onClose={() => setOpenTelegramID(false)}
+            >
+                <DialogTitle>
+                    Authenticate with Telegram
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To get your TID ( Telegram ID ), open TFA bot and click on my info. TID is available there and you can add it herel
+                    </DialogContentText>
+                    <br />
+                    <TextField
+                        variant="outlined"
+                        color="primary"
+                        label="Telegram ID"
+                        placeholder="Enter Telegram ID"
+                        size="small"
+                        type="number"
+                        onChange={(e) => setTelegramID(e.target.value)}
+                        value={telegramID}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => enableTFA()}
+                        disableElevation
+                    >
+                        Enable TFA
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Container>
     );
 }
