90c5feccc0b6874354168e185e4eb58bd898c336
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-27 08:17:10 +0430

    Home tab

---
+++ src/pages/tabs/home.jsx
@@ -0,0 +1,16 @@
+import {
+    Box,
+    Typography
+} from "@mui/material";
+
+const HomeTab = () => {
+    return (
+        <Box>
+            <Typography>
+                Home tab
+            </Typography>
+        </Box>
+    )
+}
+
+export default HomeTab;
